71ad9e4086dbcf37fc58c0ea5db2b3449f286c06
kiryu3030 <120580807+kiryu3030@users.noreply.github.com>
2025-10-28 16:47:06 +0900

    comport test

--- app/serial/rs485.js
+++ app/serial/rs485.js
@@ -80,13 +80,7 @@ class RS485Port {
   }
 }
 
-let stepperPort1 = new RS485Port('COM21', 38400);
-let stepperPort2 = new RS485Port('COM16', 38400);
-let stepperPort3 = new RS485Port('COM17', 38400);
-let servoPort1 = new RS485Port('COM18', 9600);
-let servoPort2 = new RS485Port('COM6', 9600);
-let servoResponePort1 = new RS485Port('COM25', 38400);
-let servoResponePort2 = new RS485Port('COM26', 38400);
+let stepperPort1 = new RS485Port('COM3', 38400);
 
 stepperPort1.setErrorHandler((err)=>{
   try {
@@ -96,28 +90,11 @@ stepperPort1.setErrorHandler((err)=>{
   }
 });
 
-stepperPort2.setErrorHandler((err)=>{
-  try {
-    logging.info('Stepper2Error: ', err.message);
-  } catch (error) {
-    logging.error(error);
-  }
-});
-
 const RS485 = {
   stepperPort1: stepperPort1,
-  stepperPort2: stepperPort2,
-  stepperPort3: stepperPort3,
-  servoPort1: servoPort1,
-  servoPort2: servoPort2,
   open: () => {
     stepperPort1.open();
-    stepperPort2.open();
-    stepperPort3.open();
-    servoPort1.open();
-    servoResponePort1.open();
-    servoResponePort2.open();
   },
 }
 
-export default RS485;
+module.exports = RS485;
--- webserver.js
+++ webserver.js
@@ -3,9 +3,17 @@ const logger = require('./app/config/log.config.js');
 
 const logging = logger(__filename);
 
+const RS485 = require('./app/serial/rs485.js');
+
+const { sleep } = require('./app/utilities/utils.js');
+
 // const PORT = 8084;
 // mainApp.listen(PORT, () => {
 //   logging.info(`WebServer is running on port ${PORT}`)
 // })
 
 console.log("test");
+RS485.open();
+
+sleep(5);
+
